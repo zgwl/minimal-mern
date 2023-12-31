8c035a5234bc002f59bb2f5e6bd1d4fc0df34449
Liang <kusinerloo@gmail.com>
2023-12-30 18:20:04 -0800

    add Environment Variable to distiguish prod/dev

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,15 @@
 import cors from "cors";
 import express from "express";
 
+const env = process.env.NODE_ENV || "development";
+
 const app = express();
 const port = 5050;
 
 app.use(cors());
 
 app.get("/", (_req, res) => {
-  res.send("Hello, World!");
+  res.send(`Hello, World! Current environment: ${env}`);
 });
 
 const server = app.listen(port, () => {
